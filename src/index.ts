e1c82aa1af72bda691164ae5564252702af00157
Rakin Rahman <rakinhacker@gmail.com>
2026-01-29 14:52:55 +0600

    feat: add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,22 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", (c) => c.text("Hello Node.js!"));
+
+const server = serve(app);
+
+// Graceful shutdown
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
 });
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  },
-);
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+});
