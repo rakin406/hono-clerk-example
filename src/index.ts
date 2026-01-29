6b87bbd24a021e3a6204c875045c76608b1a95c2
Rakin Rahman <rakinhacker@gmail.com>
2026-01-30 00:26:38 +0600

    feat: delete account route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { deleteCookie, setCookie } from "hono/cookie";
 import { sValidator } from "@hono/standard-validator";
 import * as z from "zod";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
-// TODO: Implement login, logout and delete user.
+// DISCLAIMER: You need a frontend.
 
 const User = z.object({
   email: z.string(),
@@ -43,24 +44,34 @@ app.post("/register", sValidator("form", User), async (c) => {
     password: data.password,
   });
 
+  const auth = getAuth(c);
+  const sessionId = auth.sessionId ?? "";
+  setCookie(c, "__session", sessionId, {
+    secure: true,
+    httpOnly: true,
+  });
+
   return c.json({ message: "You have registered!" }, 201);
 });
 
-app.post("/login", sValidator("form", User), async (c) => {
-  // const clerkClient = c.get("clerk");
-  // const data = c.req.valid("form");
-  //
-  // await clerkClient.users.createUser({
-  //   emailAddress: [data.email],
-  //   password: data.password,
-  // });
-  //
-  // return c.json({ message: "You have registered!" }, 201);
+app.delete("/delete", async (c) => {
+  const clerkClient = c.get("clerk");
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json(
+      {
+        message: "You are not logged in.",
+      },
+      401,
+    );
+  }
+
+  await clerkClient.users.deleteUser(auth.userId);
+  deleteCookie(c, "__session");
+
+  return c.json({ message: "You have deleted your account!" });
 });
-
-app.get("/logout", (c) => c.text("Logout endpoint"));
-
-app.get("/delete", (c) => c.text("Delete endpoint"));
 
 const server = serve(app);
 
